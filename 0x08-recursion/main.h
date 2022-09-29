e72c3d776732b093eba977197392639122161ef9
Joseph <ajandowa@gmail.com>
2022-09-29 01:19:01 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: me
- * Desc: Header file containing prototypes for all functions
- * used in the 0x07-recursion directory.
- */
+#include <stdio.h>
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -16,7 +11,9 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int is_palindrome(char *s);
+int _sqrt(int n, int x);
+int is_prime(int n, int start);
+void check(char *s, int start, int end, int *flag);
 int wildcmp(char *s1, char *s2);
 
-#endif
+#endif /*MAIN_H*/
