fe047d2abc55092b6293c6b0afaab44a2d44efe8
Joseph <ajandowa@gmail.com>
2022-10-11 14:09:10 -0700

    all commits

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,21 @@
-#ifndef STRTYP
-#define STRTYP
+#ifndef DOG_H_FILE
+#define DOG_H_FILE
 
 /**
- * struck dog - structure of a dog.
- * @name: name of dog.
- * @age: age of dog.
- * @owner: owner of dog.
+ * struct dog - structure definition of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-#endif /*STRTYP*/
+#endif
